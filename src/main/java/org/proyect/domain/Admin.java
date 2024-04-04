f93fd0f3a4a217ab8c75aef2194b207fb3d56780
Daniel <danielhdezgarcia9daw@gmail.com>
2024-04-04 18:39:13 +0200

    Creacion Evento

--- src/main/java/org/proyect/domain/Admin.java
+++ src/main/java/org/proyect/domain/Admin.java
@@ -1,20 +1,25 @@
 package org.proyect.domain;
 
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Admin extends Persona{
+public class Admin extends Persona {
 
     @Column
     private Long id_admin;
 
-        // ==================
+    @OneToMany(mappedBy = "admin")
+    private List<Evento> eventos;
+
+    // ==================
 
     public Admin(Long id_admin) {
         this.id_admin = id_admin;
---
+++ src/main/java/org/proyect/domain/Evento.java
@@ -0,0 +1,43 @@
+package org.proyect.domain;
+
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_evento;
+
+    @Column
+    private String tipo;
+    @Column
+    private Date fecha;
+    @Column
+    private String localizacion;
+    @ManyToOne
+    private Admin admin;
+    
+
+
+    // ==================
+
+    public Evento(String tipo, Date fecha, String localizacion) {
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.localizacion = localizacion;
+    }
+
+}
