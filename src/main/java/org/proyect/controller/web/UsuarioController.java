a70d1910486ab5886fe73887b43109c2eb86d8aa
nataliaAlcon <nata.alsal@gmail.com>
2024-04-23 18:54:48 +0200

    cambiosUsuarios

--- src/main/java/org/proyect/controller/web/UsuarioController.java
+++ src/main/java/org/proyect/controller/web/UsuarioController.java
@@ -2,9 +2,13 @@ package org.proyect.controller.web;
 
 import java.util.List;
 
+import org.proyect.domain.Juego;
+import org.proyect.domain.Pelicula;
 import org.proyect.domain.Usuario;
 import org.proyect.exception.DangerException;
 import org.proyect.helper.PRG;
+import org.proyect.repository.JuegoRepository;
+import org.proyect.repository.PeliculaRepository;
 import org.proyect.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +23,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    @Autowired
+    private JuegoRepository videojuegoRepository;
 
     @GetMapping("r")
     public String r(ModelMap m) {
@@ -46,5 +56,16 @@ public class UsuarioController {
             PRG.error("El usuario  con el nombre " + nombre + " ya existe", "/");
         }
         return "redirect:/";
+    }
+
+    @GetMapping("listar")
+    public String listar(ModelMap m) {
+        List<Pelicula> peliculas = peliculaRepository.findAll();
+        List<Juego> videojuegos = videojuegoRepository.findAll();
+
+        m.put("peliculas", peliculas);
+        m.put("videojuegos", videojuegos);
+        m.put("view", "usuario/listar");
+        return "_t/frame";
     }
 }
