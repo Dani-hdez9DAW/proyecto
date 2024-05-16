bb5cb8b5ffd3704a7d737fc2f424de947e6533bf
Jessica <dam.jessica.lopez@gmail.com>
2024-05-16 20:17:06 +0200

    arrerglado fav peliculas y juegos

--- src/main/java/org/proyect/NombreApp.java
+++ src/main/java/org/proyect/NombreApp.java
@@ -3,8 +3,10 @@ package org.proyect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableTransactionManagement
 public class NombreApp {
 
 	public static void main(String[] args) {
